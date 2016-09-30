c966989ffff2fdea76e76c09814acc7e786798ec
Andrey Beketov <pincode@yandex.ru>
2016-09-30 15:23:30 +0400

    Add ClosedXML DataBar test

--- Alfa/Alfa/Program.cs
+++ Alfa/Alfa/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClosedXML.Excel;
 
 namespace Alfa
 {
@@ -10,6 +11,18 @@ namespace Alfa
 	{
 		static void Main(string[] args)
 		{
+			XLWorkbook Book = new XLWorkbook();
+			var Page = Book.Worksheets.Add("Page1");
+			Page.Cell(1, 1).Value = 1;
+			Page.Cell(2, 1).Value = 10;
+			Page.Cell(3, 1).Value = 100;
+
+			Page.Column(1).Width = 20;
+			Page.Range(1, 1, 3, 1).AddConditionalFormat().DataBar(XLColor.Gray).LowestValue().HighestValue();
+
+			var FileName = "DataBar.xlsx";
+            Book.SaveAs(FileName);
+			System.Diagnostics.Process.Start(FileName);
 		}
 	}
 }
